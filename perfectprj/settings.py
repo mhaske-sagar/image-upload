b9b36781ecb3e43a44bf606a80fb89723e90d749
mhaske-sagar <mhaskesagar99@gmail.com>
2022-11-23 18:39:58 +0530

    setg

--- perfectprj/settings.py
+++ perfectprj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wd65@vf2fz(5q3^&r=jov^q2gn%2y-iml6p)lr1uqo1x2mfr*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#MEDIA_URL="/media/"
-#MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
