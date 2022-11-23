dfca88492e7e304f4f01d8a778a7ce3aa1dbd5dd
mhaske-sagar <mhaskesagar99@gmail.com>
2022-11-23 17:24:23 +0530

    new vercel

--- perfectprj/settings.py
+++ perfectprj/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wd65@vf2fz(5q3^&r=jov^q2gn%2y-iml6p)lr1uqo1x2mfr*k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -127,6 +127,9 @@ STATIC_URL = 'static/'
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
